fa109f38b8ad667890d0ed6b51058b9e6217827d
Reaper <rjs5785@hotmail.com>
2020-06-06 12:17:36 -0400

    have a pygame window drawing now and able to blit the first frame of the homer simpson gif onto the screen

--- Workspace/streamsavers.py
+++ Workspace/streamsavers.py
@@ -1,5 +1,19 @@
 import pygame
+import sys
 
 pygame.init() #pylint: disable=maybe-no-member
 
-print("hello world")
+size = width, height = 320, 240
+screen = pygame.display.set_mode(size)
+black = 0, 0, 0
+
+homer = pygame.image.load('C:\\Users\\rjs57\\gifs\\simpsons.gif') #pylint: disable=maybe-no-member
+homerrect = homer.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #pylint: disable=maybe-no-member
+            sys.exit()
+        screen.fill(black)
+        screen.blit(homer, homerrect)
+        pygame.display.flip()
