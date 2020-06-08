52d8334dac82499cee96f782bc45412ea89f7ffc
Reaper <rjs5785@hotmail.com>
2020-06-08 14:41:23 -0400

    am able to load gifs and play them now

--- Workspace/streamsavers.py
+++ Workspace/streamsavers.py
@@ -1,26 +1,24 @@
-from PIL import Image
-from PIL import GifImagePlugin
-import pygame
-import sys
-
-pygame.init() #pylint: disable=maybe-no-member
-
-homer = pygame.image.open('C:\\Users\\rjs57\\gifs\\simpsons.gif', 'r')
-homerrect = homer.get_rect()
-
-size = width, height = homer.get_rect().x, homer.get_rect().y
-screen = pygame.display.set_mode(homer.get_rect().size)
-black = 0, 0, 0
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #pylint: disable=maybe-no-member
-            running = False
-        
-        for frame in range(0, homer.n_frames):
-            screen.fill(black)
-            homer.seek(frame)
-            screen.blit(homer, homerrect) #pylint: disable=maybe-no-member
-            pygame.display.flip()
-
+import pyglet
+ 
+animation = pyglet.image.load_animation('gifs/simpsons.gif')
+animSprite = pyglet.sprite.Sprite(animation)
+ 
+ 
+w = animSprite.width
+h = animSprite.height
+ 
+window = pyglet.window.Window(width=w, height=h)
+ 
+r,g,b,alpha = 0.5,0.5,0.8,0.5
+ 
+ 
+pyglet.gl.glClearColor(r,g,b,alpha)
+ 
+@window.event
+def on_draw():
+    window.clear()
+    animSprite.draw()
+ 
+ 
+ 
+pyglet.app.run()
