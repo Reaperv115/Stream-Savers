86664a52582190d5908270eef961e77baef2e694
Reaper <rjs5785@hotmail.com>
2020-06-09 14:48:05 -0400

    trying to figure out how the main loop works for pyglet to see if I can create a pause after a gif has fully played once

--- Workspace/streamsavers.py
+++ Workspace/streamsavers.py
@@ -1,9 +1,10 @@
 import pyglet
 import os
 from os import listdir
+import time
  
 listdirs = os.listdir('C:\\Users\\rjs57\\gifs')
-i = 0
+i = 3
 timer = 0.0
 
 animation = pyglet.image.load_animation('C:\\Users\\rjs57\\gifs\\' + listdirs[i])
@@ -17,6 +18,7 @@ window = pyglet.window.Window(width=w, height=h)
  
 r,g,b,alpha = 0.5,0.5,0.8,0.5
  
+event_loop = pyglet.app.EventLoop()
  
 pyglet.gl.glClearColor(r,g,b,alpha)
  
@@ -24,11 +26,25 @@ pyglet.gl.glClearColor(r,g,b,alpha)
 def on_draw():
     window.clear()
     animSprite.draw()
+    
+@event_loop.event
+def onWindowClose(window):
+    event_loop.exit()
+    return pyglet.event.EVENT_HANDLED
 
-    # if timer >= 5.0:
-    #     if i > listdirs.__sizeof__:
-    #     i += 1
+
+while True:
+    pyglet.clock.tick()
+
+    for window in pyglet.app.windows:
+        event_loop.run()
+        window.switch_to()
+        window.dispatch_events()
+        window.dispatch_event('on_draw')
+        window.dispatch_event('onWindowClose')
+        window.flip()
+
+    
  
  
- 
-pyglet.app.run()
+#pyglet.app.run()
