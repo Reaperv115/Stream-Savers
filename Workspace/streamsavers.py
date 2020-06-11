0656b343a4e19b0462822beb9a1cfabab58412c3
Reaper <rjs5785@hotmail.com>
2020-06-11 10:11:08 -0400

    committing to push

--- Workspace/streamsavers.py
+++ Workspace/streamsavers.py
@@ -4,47 +4,38 @@ from os import listdir
 import time
  
 listdirs = os.listdir('C:\\Users\\rjs57\\gifs')
-i = 3
-timer = 0.0
+print(listdirs)
+# i = 0
+# timer = 0.0
 
-animation = pyglet.image.load_animation('C:\\Users\\rjs57\\gifs\\' + listdirs[i])
-animSprite = pyglet.sprite.Sprite(animation)
+# animation = pyglet.image.Animation.from_image_sequence('C:\\Users\\rjs57\\gifs\\' + listdirs[i], 5, True)
+# animSprite = pyglet.sprite.Sprite(animation)
  
  
-w = animSprite.width
-h = animSprite.height
+# w = animSprite.width
+# h = animSprite.height
  
-window = pyglet.window.Window(width=w, height=h)
+# window = pyglet.window.Window(width=w, height=h)
  
-r,g,b,alpha = 0.5,0.5,0.8,0.5
+# r,g,b,alpha = 0.5,0.5,0.8,0.5
  
-event_loop = pyglet.app.EventLoop()
+# event_loop = pyglet.app.EventLoop()
  
-pyglet.gl.glClearColor(r,g,b,alpha)
+# pyglet.gl.glClearColor(r,g,b,alpha)
+
+# @window.event
+# def nextAnim():
+#     if animSprite.on_animation_end(animSprite):
+#         i += 1
  
-@window.event
-def on_draw():
-    window.clear()
-    animSprite.draw()
+# @window.event
+# def on_draw():
+#     window.clear()
+#     animSprite.draw()
     
-@event_loop.event
-def onWindowClose(window):
-    event_loop.exit()
-    return pyglet.event.EVENT_HANDLED
+# @event_loop.event
+# def on_window_close(window):
+#     event_loop.exit()
+#     return pyglet.event.EVENT_HANDLED
 
-
-while True:
-    pyglet.clock.tick()
-
-    for window in pyglet.app.windows:
-        event_loop.run()
-        window.switch_to()
-        window.dispatch_events()
-        window.dispatch_event('on_draw')
-        window.dispatch_event('onWindowClose')
-        window.flip()
-
-    
- 
- 
-#pyglet.app.run()
+# pyglet.app.run()
