61738e65ec4b7052a692ca342d38af7520bb2752
Reaper <rjs5785@hotmail.com>
2020-06-18 09:53:45 -0400

    committing to push

--- streamsavers.py
+++ streamsavers.py
@@ -4,41 +4,19 @@ import imageio
  
 image_path = Path('gifs')
 images = list(image_path.glob('*.gif'))
+print(images)
 
 
-animation = pyglet.sprite.Sprite(images[0])
+# animation = pyglet.sprite.Sprite(images[0])
 
-width = animation.width
-height = animation.height
+# width = animation.width
+# height = animation.height
 
-window = pyglet.window.Window(width, height)
+# window = pyglet.window.Window(width, height)
 
-@window.event
-def onDraw():
-    window.clear()
-    animation.draw()
-
-pyglet.app.run()
-
-# animation = pyglet.image.load_animation('gifs/homer.gif')
-# animSprite = pyglet.sprite.Sprite(animation)
- 
- 
-# w = animSprite.width
-# h = animSprite.height
- 
-# window = pyglet.window.Window(width=w, height=h)
- 
-# r,g,b,alpha = 0.5,0.5,0.8,0.5
- 
- 
-# pyglet.gl.glClearColor(r,g,b,alpha)
- 
 # @window.event
-# def on_draw():
+# def onDraw():
 #     window.clear()
-#     animSprite.draw()
- 
- 
- 
+#     animation.draw()
+
 # pyglet.app.run()
