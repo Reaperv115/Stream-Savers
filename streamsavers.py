8c754364efcb805af2f5546d78f663fdd63b3d1d
Reaper <rjs5785@hotmail.com>
2020-06-24 16:52:32 -0400

    finished the Stream Savers program for now

--- streamsavers.py
+++ streamsavers.py
@@ -6,49 +6,34 @@ import os
 
 
 def main():
-    path = 'D:\\Dev\\StreamSavers\\gifs\\*.gif'
-    event_loop = pyglet.app.EventLoop()
+    path = 'E:\\Dev\\StreamSavers\\gifs\\1.gif'
 
-    gifs = glob.glob(path, recursive=False)
-    gifnum = 0
-
-    event_loop = pyglet.app.base.EventLoop()
-        
-    animation = pyglet.image.load_animation(gifs[gifnum])
-
-    abspath = os.path.abspath(gifs[gifnum])
-    duration = Image.open(abspath).info['duration']
-
+    animation = pyglet.image.load_animation(path)
     animSprite = pyglet.sprite.Sprite(animation)
-
-
+    
+    duration = Image.open(path).info['duration']
+    
     w = animSprite.width
     h = animSprite.height
-
+    
     window = pyglet.window.Window(width=w, height=h)
-
+    
     r,g,b,alpha = 0.5,0.5,0.8,0.5
-
-
+    
+    
     pyglet.gl.glClearColor(r,g,b,alpha)
-
+    
     @window.event
     def on_draw():
         window.clear()
         animSprite.draw()
-        
-
-    @event_loop.event
-    def on_Window_close():
-        exit()
 
     def pauseAnim(self):
-        time.sleep(4.0)
-        
-
+         time.sleep(4.0)
+    
     pyglet.clock.schedule_interval(pauseAnim, duration)
-
-    pyglet.app.EventLoop().run()
+    
+    pyglet.app.run()
 
 if __name__ == '__main__':
     main()
