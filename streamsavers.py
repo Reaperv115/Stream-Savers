2640e3ceed7eca2cece117b8e6caf107c24c832b
Reaper <rjs5785@hotmail.com>
2020-07-10 12:00:35 -0400

    commented everything

--- streamsavers.py
+++ streamsavers.py
@@ -4,20 +4,32 @@ import pyglet
 import glob
 import keyboard
 
+#loading all video-type files 
 types = ('*.gif', '*.webm', '*.mpeg')
 gifs = []
 for files in types:
     gifs.extend(glob.glob(files))
+
+#video index
 gifnum = 0
-w, h = 600, 600
+
+#video stream
 cap = cv2.VideoCapture(gifs[gifnum])
+
+#pause image
 black = cv2.imread('black.png')
 
+#making sure the video stream was successfully opened
 if (cap.isOpened() == False):
     print("failed to open file")
+
 while (cap.isOpened()):
+
+    #ret is true if you returned with a valid frame, otherwise false
+    #frame is the actual frame data to display
     ret, frame = cap.read()
 
+    #checking for keyboard input to change video
     if (keyboard.is_pressed('n')):
         gifnum += 1
         if (gifnum >= len(gifs)):
@@ -31,11 +43,13 @@ while (cap.isOpened()):
         cap = cv2.VideoCapture(gifs[gifnum])
         ret, frame = cap.read()
 
+    #if ret is true display the video frame
     if (ret == True):
         cv2.namedWindow('video', cv2.WND_PROP_FULLSCREEN)
         cv2.imshow('video', frame)
         if (cv2.waitKey(50) & 0xFF == ord('q')):
             break
+    #else pause on the black screen and start next video
     else:
         cap.release()
         cv2.imshow('video', black)
@@ -46,6 +60,6 @@ while (cap.isOpened()):
             gifnum = 0
         cap = cv2.VideoCapture(gifs[gifnum])
         
-
+# close video stream and destroy all windows
 cap.release()
 cv2.destroyAllWindows()
